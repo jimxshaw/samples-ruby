19faf563a9f8748f9e8cb24ef95ed1ddecf85aa2
Jim Shaw <jimxshaw@gmail.com>
2017-07-13 08:42:30 -0500

    Misc items.

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+    # Convention dictates the actions in the following order:
+    # index, show, new, edit, create, update, destroy
+
+
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+
+        redirect_to article_path(@article)
+    end
+
+
+    private
+    def comment_params
+        params.require(:comment).permit(:commenter, :body)
+    end
 end
