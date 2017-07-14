2f535db526c76c15b9d667bcaa5e1363d8be0498
Jim Shaw <jimxshaw@gmail.com>
2017-07-14 10:53:51 -0500

    Update static pages views in line with tests.

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  app_name = "Ruby on Rails Tutorial Sample App"
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | #{app_name}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | #{app_name}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | #{app_name}"
   end
 
 end
