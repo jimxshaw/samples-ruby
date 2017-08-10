d291a58b15a0949b7140fcb797bf1afe94409dca
Jim Shaw <jimxshaw@gmail.com>
2017-08-10 16:24:57 -0500

    DevcampPortfolio create seeds data in seeds.rb.

--- DevcampPortfolio/db/seeds.rb
+++ DevcampPortfolio/db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+      title: "My Blog Post #{blog}",
+      body: "Moleskine ship it responsive sticky note pair programming innovate user centered design parallax entrepreneur pitch deck. Sticky note waterfall is so 2000 and late integrate experiential responsive fund pitch deck viral ideate parallax SpaceTeam engaging. User centered design personas bootstrapping pair programming parallax ship it co-working 360 campaign ship it. Minimum viable product ideate physical computing co-working thinker-maker-doer parallax pitch deck latte prototype responsive venture capital entrepreneur."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+      title: "Skill #{skill}",
+      percent_utilized: 20
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+     title: "Portfolio title #{portfolio_item}",
+     subtitle: "This is the subtitle",
+     body: "Paradigm venture capital co-working big data user centered design pair programming workflow 360 campaign. Entrepreneur pitch deck paradigm disrupt workflow agile driven hacker.",
+     main_image: "http://via.placeholder.com/600x400",
+     thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+puts "9 portfolio items created"
