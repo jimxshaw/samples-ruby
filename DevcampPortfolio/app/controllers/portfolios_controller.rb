0605cb24606863e9e64cff87fed75c91b9af0c49
Jim Shaw <jimxshaw@gmail.com>
2017-08-11 11:33:33 -0500

    DevcampPortfolio insert dynamic links in index action.

--- DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -2,7 +2,10 @@ class PortfoliosController < ApplicationController
 
   def index
     @portfolio_items = Portfolio.all
+  end
 
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def new
@@ -27,7 +30,7 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html {redirect_to portfolios_path, notice: 'The record was successfully updated.'}
