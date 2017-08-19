fa3c4da57ee72a1dd55166d7a3a916c125c2f26f
Jim Shaw <jimxshaw@gmail.com>
2017-08-19 17:54:28 -0500

    RoutingApp test out routes.

--- RoutingApp/config/routes.rb
+++ RoutingApp/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
+
+  get 'about', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
+  resources :posts
+
+  root to: 'pages#home'
 
 end
